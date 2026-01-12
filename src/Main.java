2b6e903966f82806b9e17a58b9fc3cb97b0d7edc
arifmaulanaaaa <arif270505@gmail.com>
2026-01-12 14:58:15 +0700

    Menambahkan saran kepada pengguna.

--- src/Main.java
+++ src/Main.java
@@ -8,24 +8,35 @@ public class Main {
         while (lanjut) {
             try {
                 System.out.println("=== Kalkulator BMI ===");
+
                 System.out.print("Masukkan berat badan (Kg): ");
                 double berat = input.nextDouble();
 
                 System.out.print("Masukkan tinggi badan (cm): ");
                 double tinggi = input.nextDouble();
 
-                double tinggiBadanKuadrat = tinggi * tinggi;
-                double bmi = berat / tinggiBadanKuadrat;
+                double tinggiMeter = tinggi / 100;
+                double bmi = berat / (tinggiMeter * tinggiMeter);
+                System.out.printf("Skor bmi anda: %.2f\n", bmi);
+
+                String saran;
 
                 if (bmi < 18.5) {
                     System.out.println("Berat badan anda kurang.");
+                    saran = "Tambah asupan makanan bergizi cuy";
                 } else if (bmi >= 18.5 && bmi <= 22.9) {
                     System.out.println("Berat badan anda normal.");
+                    saran = "Pertahankan cuy";
                 } else if (bmi >= 23 && bmi <= 29.9) {
                     System.out.println("Berat badan anda berlebih.");
+                    saran = "Defisit kalori dan perbanyak gerak bro!";
                 } else {
                     System.out.println("Anda obesitas.");
+                    saran = "Defisit kalori dan perbanyak gerak bro!";
                 }
+
+                System.out.println("Saran: " + saran + "\n");
+
             } catch (Exception e) {
                 System.out.println("Input tidak valid! harus input angka.");
                 input.nextLine();
